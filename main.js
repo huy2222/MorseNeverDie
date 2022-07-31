38afacefeda48434ae612ac4258ca50629fbaeda
huy2222 <huynhatnguyen246@gmail.com>
2022-07-31 11:40:25 +0700

    my change

--- main.js
+++ main.js
@@ -14,11 +14,9 @@ var resetprivate=document.getElementById('resetprivate');
 var private=document.getElementById('private');
 var array=[];
 var arrayShow=[];
-var MyTimeour = setTimeout(MyInfor,3000);
+var number=0;
+ metachange.value='TUWFAAN';
 
-function MyInfor() {  
-    private.value="Nhật Huy đến từ GĐPT Từ ÂN";
-}
 
 function TichFunction() {
    array.push(tich.value);
@@ -90,6 +88,7 @@ function ArrayFunction () {
         if (result=='.--.' ) { arrayShow.push('P')} else 
         if (result=='-.-' ) { arrayShow.push('K')} else 
         if (result=='.--' ) { arrayShow.push('W')} else 
+        if (result=='-..-' ) { arrayShow.push('X')} else 
         if (result=='--.' ) { arrayShow.push('G')} else 
         if (result=='.-..' ) { arrayShow.push('L')} else 
         if (result=='..-.' ) { arrayShow.push('F')} else 
@@ -130,6 +129,7 @@ function showFunction() {
         if (result=='.--.' ) { private.value='P'} else 
         if (result=='-.-' ) { private.value='K'} else 
         if (result=='.--' ) { private.value='W'} else 
+        if (result=='-..-' ) { private.value='X'} else 
         if (result=='--.' ) { private.value='G'} else 
         if (result=='.-..' ) { private.value='L'} else 
         if (result=='..-.' ) { private.value='F'} else 
@@ -155,22 +155,126 @@ function ResetFunction() {
 reset.addEventListener('click',ResetFunction)
 
 
-var ContentMorse = document.getElementById('Id-content');
-var Password = document.getElementById('password');
-var btnPassword = document.getElementById('submit-password');
-var ContentLayout = document.getElementById('JS-layout');
+// var ContentMorse = document.getElementById('Id-content');
+// var Password = document.getElementById('password');
+// var btnPassword = document.getElementById('submit-password');
+// var ContentLayout = document.getElementById('JS-layout');
 
-function GetvaluePassword() {
-    var passwordValue= Password.value;
-    if (passwordValue=='gdpttuan') {
-        ContentMorse.style.display='block';
-        ContentLayout.style.display='none';
-        
-    } else {
-        alert('bạn nhập sai mật khẩu');
+// function GetvaluePassword() {
+//     var passwordValue= Password.value;
+//     if (passwordValue=='gdpttuan') {
+//         ContentMorse.style.display='block';
+//         ContentLayout.style.display='none';
+//         var MyTimeour = setTimeout(MyInfor,3000);
+//         function MyInfor() {  
+//         private.value="Nhật Huy đến từ GĐPT Từ ÂN";
+// }
+//     } else {
+//         alert('bạn nhập sai mật khẩu');
+//     }
+
+// }
+// btnPassword.addEventListener('click',GetvaluePassword);
+
+
+// meta change
+var MetaChangeInput = document.getElementById('metachange');
+var btnChange=document.getElementById('btn-change');
+var RestMorseChange = document.getElementById('resetmorsechange');
+var ReseAllChange = document.getElementById('reset-change');
+function ChangeMorse() {
+    metachange.value=metachange.value.toUpperCase();
+    metachange.value=metachange.value.split(" ").join("");
+    for (var i=0; i<metachange.value.length; i++) {
+        console.log(metachange.value[i]);
+        if (metachange.value[i]=='E') { array.push('.'); } else
+        if (metachange.value[i]=='I') { array.push('..'); } else
+        if (metachange.value[i]=='S') { array.push('...'); } else
+        if (metachange.value[i]=='H') { array.push('....'); } else
+        if (metachange.value[i]=='T') { array.push('-'); } else
+        if (metachange.value[i]=='M') { array.push('--'); } else
+        if (metachange.value[i]=='O') { array.push('---'); } else
+        if (metachange.value[i]=='CH') { array.push('----'); } else
+        if (metachange.value[i]=='A') { array.push('.-'); } else
+        if (metachange.value[i]=='N') { array.push('-.'); } else
+        if (metachange.value[i]=='U') { array.push('..-'); } else
+        if (metachange.value[i]=='D') { array.push('-..'); } else
+        if (metachange.value[i]=='V') { array.push('...-'); } else
+        if (metachange.value[i]=='B') { array.push('-...'); } else
+        if (metachange.value[i]=='R') { array.push('.-.'); } else
+        if (metachange.value[i]=='P') { array.push('.--.'); } else
+        if (metachange.value[i]=='K') { array.push('-.-'); } else
+        if (metachange.value[i]=='W') { array.push('.--'); } else
+        if (metachange.value[i]=='X') { array.push('-..-'); } else
+        if (metachange.value[i]=='G') { array.push('--.'); } else
+        if (metachange.value[i]=='L') { array.push('.-..'); } else
+        if (metachange.value[i]=='F') { array.push('..-.'); } else
+        if (metachange.value[i]=='Y') { array.push('-.--'); } else
+        if (metachange.value[i]=='Q') { array.push('--.-'); } else
+        if (metachange.value[i]=='C') { array.push('-.-.'); } else
+        if (metachange.value[i]=='J') { array.push('.---'); } else
+        if (metachange.value[i]=='Z') { array.push('--..'); } else {array.push('????'); }     
     }
-
+   
+    for (var i=0; i<metachange.value.length; i++) {
+        var htmlAnpha =`<div class="child-anpha" id="${i}"></div>`;     
+        document.getElementById('result-anpha').insertAdjacentHTML('beforeend',htmlAnpha)
+        document.getElementById(i).innerHTML=`${metachange.value[i]}`; 
+        number=number+1;    
+    }
+    for (var j=metachange.value.length; j<metachange.value.length*2; j++) {
+        var htmlMorse =`<div class="child-morse" id="${j}"></div>`;
+        document.getElementById('result-morse').insertAdjacentHTML('beforeend',htmlMorse);         
+    }    
+    for (var k=0; k<array.length; k++) {     
+        document.getElementById(k+metachange.value.length).innerHTML=array[k];
+    }
+    array=[]
 }
-btnPassword.addEventListener('click',GetvaluePassword);
+btnChange.addEventListener('click',ChangeMorse)
+// number: số khối div tạo ra
+// document.getElementById("btn-change").disabled = true; //khóa nút bấm
+function MetaChange() {
+    for (var h=0; h<number; h++) {
+        var resultAnpha = document.querySelector('.child-anpha');
+        resultAnpha.parentNode.removeChild(resultAnpha);
+    }
+    for (var l=0; l<number; l++) {
+        var resultMorse = document.querySelector('.child-morse');
+        resultMorse.parentNode.removeChild(resultMorse);
+    }
+    number=0;
+    for( var x=1; x<2; x++) { 
+        var PermanentClass = document.querySelector('.permanent');
+        PermanentClass.parentNode.removeChild(PermanentClass);
+    }
+    
+    // document.getElementById("btn-change").disabled = false; // mở nút bấm
+   
+}
 
+MetaChangeInput.addEventListener('change',MetaChange)
 
+function ResetChangeInput() {
+    metachange.value='';
+    array=[];
+}
+RestMorseChange.addEventListener('click',ResetChangeInput)
+function ReseAllChangeFn() {
+    metachange.value='';
+    array=[];
+    for (var h=0; h<number; h++) {
+        var resultAnpha = document.querySelector('.child-anpha');
+        resultAnpha.parentNode.removeChild(resultAnpha);
+    }
+    for (var l=0; l<number; l++) {
+        var resultMorse = document.querySelector('.child-morse');
+        resultMorse.parentNode.removeChild(resultMorse);
+    }
+    number=0;
+    for( var x=1; x<2; x++) { 
+        var PermanentClass = document.querySelector('.permanent');
+        PermanentClass.parentNode.removeChild(PermanentClass);
+    }
+}
+ReseAllChange.addEventListener('click',ReseAllChangeFn)
